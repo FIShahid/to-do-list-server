09f3faa53392b46f8c2d55e37b6a815c83567ff4
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-19 04:28:16 +0600

    server setup

--- index.js
+++ index.js
@@ -11,10 +11,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9zoc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('Mongodb Connected');
+    const taskCollection = client.db('ToDoList').collection('taskDetails');
+
+    app.get('/todo', async (req, res) => {
+      const todo = await taskCollection.find().toArray();
+      res.send(todo);
+    });
+
+    app.post('/tasks', async (req, res) => {
+      const todo = req.body;
+      console.log(todo)
+      const result = await taskCollection.insertOne(todo);
+      res.send(result);
+     
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send('To Do Running..');
+  res.send('To-Do Server Running..');
 });
 
 app.listen(port, () => {
-  console.log('To Do Server Running on Port..' , port);
+  console.log('To-Do Server Running..');
 });
